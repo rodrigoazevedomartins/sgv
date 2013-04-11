043b6b42ebfb68079e4d6cb885506ce52acd9e25
Rodrigo <Rodrigo@Rodrigo-PC>
2013-04-10 23:38:54 -0300

    Commit Inicial

---
+++ TrabalhoLTP3/src/br/edu/ifnmg/tads/trabalholtp3/InterfaceUsuario/frmPrincipal.java
@@ -0,0 +1,19 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package br.edu.ifnmg.tads.trabalholtp3.InterfaceUsuario;
+
+/**
+ *
+ * @author Rodrigo
+ */
+public class frmPrincipal {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+}
