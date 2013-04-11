9d4d49763b2fb524f657909bd2a323498376fb17
Rodrigo <Rodrigo@Rodrigo-PC>
2013-04-11 08:21:31 -0300

    Alteração da Classe Pessoa

--- TrabalhoLTP3/src/br/edu/ifnmg/tads/trabalholtp3/DomainModel/Pessoa.java
+++ TrabalhoLTP3/src/br/edu/ifnmg/tads/trabalholtp3/DomainModel/Pessoa.java
@@ -22,6 +22,86 @@ public class Pessoa {
     private List<Endereco> enderecos;
     private List<Email> emails;
     private List<Telefone> telefones;
+
+    public int getCodpessoa() {
+        return codpessoa;
+    }
+
+    public void setCodpessoa(int codpessoa) {
+        this.codpessoa = codpessoa;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public String getNomepai() {
+        return nomepai;
+    }
+
+    public void setNomepai(String nomepai) {
+        this.nomepai = nomepai;
+    }
+
+    public String getRg() {
+        return rg;
+    }
+
+    public void setRg(String rg) {
+        this.rg = rg;
+    }
+
+    public int getCpf() {
+        return cpf;
+    }
+
+    public void setCpf(int cpf) {
+        this.cpf = cpf;
+    }
+
+    public Date getDatanasc() {
+        return datanasc;
+    }
+
+    public void setDatanasc(Date datanasc) {
+        this.datanasc = datanasc;
+    }
+
+    public String getNaturalidade() {
+        return naturalidade;
+    }
+
+    public void setNaturalidade(String naturalidade) {
+        this.naturalidade = naturalidade;
+    }
+
+    public List<Endereco> getEnderecos() {
+        return enderecos;
+    }
+
+    public void setEnderecos(List<Endereco> enderecos) {
+        this.enderecos = enderecos;
+    }
+
+    public List<Email> getEmails() {
+        return emails;
+    }
+
+    public void setEmails(List<Email> emails) {
+        this.emails = emails;
+    }
+
+    public List<Telefone> getTelefones() {
+        return telefones;
+    }
+
+    public void setTelefones(List<Telefone> telefones) {
+        this.telefones = telefones;
+    }
     
     
 
