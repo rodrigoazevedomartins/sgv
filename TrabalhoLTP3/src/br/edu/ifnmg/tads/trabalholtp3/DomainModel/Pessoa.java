2bcbd444efb7ef364a02b5b141d93fdb0517ca46
Rodrigo <Rodrigo@Rodrigo-PC>
2013-04-11 00:45:45 -0300

    Criação das Classes Pessoa, Endereco, Email e Telefone

---
+++ TrabalhoLTP3/src/br/edu/ifnmg/tads/trabalholtp3/DomainModel/Pessoa.java
@@ -0,0 +1,28 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package br.edu.ifnmg.tads.trabalholtp3.DomainModel;
+
+import java.util.Date;
+import java.util.LinkedList;
+import java.util.List;
+/**
+ *
+ * @author Rodrigo
+ */
+public class Pessoa {
+    private int codpessoa;
+    private String nome;
+    private String nomepai;
+    private String rg;
+    private int cpf;
+    private Date datanasc;
+    private String naturalidade;
+    private List<Endereco> enderecos;
+    private List<Email> emails;
+    private List<Telefone> telefones;
+    
+    
+
+}
