e48d5c0465a15b730d469b317413e24a50b296a8
Rodrigo <Rodrigo@Rodrigo-PC>
2013-04-20 23:31:07 -0300

    Alteração na Classe Endereco

--- TrabalhoLTP3/src/br/edu/ifnmg/tads/trabalholtp3/DomainModel/Endereco.java
+++ TrabalhoLTP3/src/br/edu/ifnmg/tads/trabalholtp3/DomainModel/Endereco.java
@@ -38,48 +38,72 @@ public class Endereco {
         return codendereco;
     }
 
-    public void setCodendereco(int codendereco) {
-        this.codendereco = codendereco;
+    public void setCodendereco(int codendereco) throws ErroValidacaoException{
+        if (codendereco > 0){
+            this.codendereco = codendereco;    
+        } else {
+            throw new ErroValidacaoException("Código Endereço Inválido. Insira um código maior que 0.");
+        }
+        
     }
 
     public String getRua() {
         return rua;
     }
 
-    public void setRua(String rua) {
-        this.rua = rua;
+    public void setRua(String rua) throws ErroValidacaoException{
+        if ((rua.length() >= 3) && (rua.length() <= 250)){
+            this.rua = rua;
+        } else {
+            throw new ErroValidacaoException("Rua Inválida. Insira uma rua com no minimo 3 caracteres e com no máximo 250 caracteres.");
+        }
     }
 
     public int getNumero() {
         return numero;
     }
 
-    public void setNumero(int numero) {
-        this.numero = numero;
+    public void setNumero(int numero) throws ErroValidacaoException {
+        if (numero > 0) {
+            this.numero = numero;
+        } else {
+            throw new ErroValidacaoException("Número Inválido. Insira um número maior que 0.");
+        }
     }
-
     public String getComplemento() {
         return complemento;
     }
 
-    public void setComplemento(String complemento) {
-        this.complemento = complemento;
+    public void setComplemento(String complemento) throws ErroValidacaoException {
+        if ((complemento.length() >= 3) && (complemento.length() <= 250)){ 
+            this.complemento = complemento;
+        } else {
+            throw new ErroValidacaoException("Complemento Inválido. Insira um complemento com no minimo 3 caracteres e com no máximo 250 caracteristicas.");
+        }
     }
-
+    
     public String getBairro() {
         return bairro;
     }
 
-    public void setBairro(String bairro) {
-        this.bairro = bairro;
+    public void setBairro(String bairro) throws ErroValidacaoException{
+        if ((bairro.length() >= 3) && (bairro.length() <= 250)){    
+            this.bairro = bairro;
+        } else {
+            throw new ErroValidacaoException("Baiiro Inválido. Insira um bairro com no minimo 3 caracteres e com no máximo 250 caracteristicas.");
+        } 
     }
-
     public String getCidade() {
         return cidade;
     }
 
-    public void setCidade(String cidade) {
-        this.cidade = cidade;
+    public void setCidade(String cidade) throws ErroValidacaoException {
+        if ((cidade.length() >= 3) && (cidade.length() <= 250)){
+            this.cidade = cidade;    
+        } else {
+            throw new ErroValidacaoException("Cidade Inválida. Insira uma cidade com no minimo 3 caracteres e com no máximo 250 caracteristicas.");
+        }
+        
     }
 
     public int getCep() {
@@ -94,18 +118,26 @@ public class Endereco {
         return estado;
     }
 
-    public void setEstado(String estado) {
-        this.estado = estado;
+    public void setEstado(String estado) throws ErroValidacaoException{
+        if ((estado.length()>=3) && (estado.length() <= 250)){
+            this.estado = estado;
+        } else {
+            throw new ErroValidacaoException("Estado Inválido. Insira um estado com no minimo 3 caracteres e com no máximo 250 caracteristicas.");
+        }
     }
 
     public String getPais() {
         return pais;
     }
 
-    public void setPais(String pais) {
-        this.pais = pais;
+    public void setPais(String pais) throws ErroValidacaoException{
+        if ((pais.length()>=3) && (pais.length() <= 250)){
+            this.pais = pais;
+        } else {
+            throw new ErroValidacaoException("País Inválido. Insira um país com no minimo 3 caracteres e com no máximo 250 caracteristicas.");
+        }
     }
-
+    
     public Pessoa getPessoa() {
         return pessoa;
     }
